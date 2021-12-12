90e0647c811fb7eb4212422cf218d88814154324
Tatjana Andrjushina <tatjana.andrjushina@gmail.com>
2021-12-12 13:13:59 +0100

    Removing everything that was added automatically by github

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:14
-WORKDIR /app
-COPY package*.json* ./
-RUN npm install 
-RUN mkdir logs
-RUN mkdir shared 
-COPY . .
--- test/helpers/envreader.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-import dotenv from 'dotenv';
-
-// no logger here, console only
-
-const dotenv_config = dotenv.config()
-export const env_file_content = dotenv_config.parsed;
-//console.debug("Env file content: " + JSON.stringify(env_file_content));
